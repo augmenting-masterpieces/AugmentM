5cde6ca26217505bc7997ee3579c78f854fdb46b
Zaturrby <rjkorteschiel@gmail.com>
2014-09-18 16:42:48 +0200

    small changes

--- src/scripts/templates.js
+++ src/scripts/templates.js
@@ -22,78 +22,19 @@ angular.module("templates/case-study.html", []).run(["$templateCache", function(
     "</section>\n" +
     "<article >\n" +
     "	<hr>\n" +
-    "	<h3> The intro text </h3>\n" +
+    "	<h3> How do visitors experience the Rijksmuseum?</h3>\n" +
     "	<hr>\n" +
-    "	<p class=\"boldAlinea\">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti cum modi error qui minima nam quae incidunt provident minus et. Neque cumque, totam mollitia illum sed excepturi error expedita. Molestiae.</p>\n" +
+    "	<p class=\"boldAlinea\">\"When I have a camera in my hand, I dare to walk in front of a painting. It's like having permission to crawl through the masses.” - participant UCS\"</p>\n" +
     "\n" +
-    "	<p>Dolor sit amet, consectetur adipisicing elit. Fugiat magni quam recusandae dolores nihil sed molestias quaerat neque corporis iste, dolorem earum aut rem natus atque eius ad animi ut, vitae nemo cum, at incidunt! Dignissimos aperiam, dolor. Sed dicta adipisci reiciendis, eum fugiat dolorem asperiores a ut. Sit assumenda neque eius reprehenderit maxime deleniti voluptate, praesentium error asperiores ratione! Reiciendis cumque obcaecati sint sapiente cum, adipisci, libero dolorem mollitia itaque dolor natus necessitatibus in eaque facilis distinctio quisquam. Ullam debitis porro corrupti, tenetur, distinctio, sunt cum laudantium obcaecati sint, incidunt cupiditate \n" +
+    "	<p>Augmenting Masterpieces applies visitor's photography through User Camera Studies (UCS)1 as one of the main research sources. In addition to other interview methods they give not only room to individual oral, but also to visual stories and analysis which supports the project's targeted innovation process for multi-medial and digital interfaces for museums. \n" +
     "	<img class=\"clearfix\" src=\"./images/ucs/1mrB/DSC00148.JPG\">\n" +
     "	</p>\n" +
     "\n" +
-    "	<p>laboriosam soluta architecto culpa aut facilis dolorem commodi! Non beatae delectus facilis quae omnis ipsam incidunt dolorum esse culpa vitae quibusdam minus doloribus consequatur eligendi voluptas facere, fuga reiciendis natus, blanditiis temporibus? Sint dignissimos quas ullam officia, ex vitae perspiciatis reprehenderit. Sit repudiandae asperiores corporis quidem sunt placeat accusantium ea earum libero eius debitis fugit itaque ex id sapiente perspiciatis, sint animi exercitationem iusto cumque quae. </p>\n" +
+    "	<p>The UCS helped to understand the social dimensions of a museum and map the museum experience as the participating visitors moved social aspects and rather basic preconditions for art reception back into the center of attention by addressing their interaction with the space, museum staff, other visitors and art objects. </p>\n" +
     "\n" +
-    "	<p>Cupiditate consequatur natus, quidem magnam accusantium quae temporibus corporis repudiandae eaque necessitatibus unde ab quod molestiae, cum tenetur, sint distinctio dolor, numquam officiis reiciendis error! Aliquid illo in assumenda quos repellat! Nobis, voluptate nihil! Tempora, consequatur cumque assumenda non dolorem debitis nam qui ullam repudiandae repellat necessitatibus quia quidem dolore recusandae odio culpa minima et laborum, ad voluptatum excepturi tenetur velit? Ipsam unde vel atque harum sit nulla, aliquam dolores animi voluptatem dolorem quas, vitae doloribus velit earum distinctio nostrum porro ullam error facilis iure aperiam, saepe repellat et. Architecto eum commodi facere ut dolor, voluptate tempore voluptatem vel rerum. Nam hic velit numquam eos quo fugiat modi, sequi aliquam libero, quam qui ipsum doloremque dicta recusandae neque, deleniti ipsam laborum esse minima at dolorum laudantium? At magnam soluta quidem rerum labore, nihil maiores praesentium, eum quas ad rem officiis, laboriosam non placeat minus.</p>\n" +
-    "	<hr>\n" +
-    "</article>\n" +
+    "	<p>This exemplary case study shows how the User Camera Studies were conducted,  givesphotographic examples and types of insights museums can gain by using photographic means as a source of feedback from their visitors.</p>\n" +
     "\n" +
-    "<section class=\"casePersons\">\n" +
-    "	<div class=\"identContainer clearfix\">\n" +
-    "\n" +
-    "		<div class=\"nameDiv clearfix\"> \n" +
-    "			<img src=\"./images/icons/male-icon.png\" alt=\"\">\n" +
-    "			<h2> Mister Bernard </h2>\n" +
-    "			<p> Age: 25 </p>\n" +
-    "			\n" +
-    "			<p> Accompanied by: Friend </p>\n" +
-    "		</div>\n" +
-    "\n" +
-    "		<div class=\"statsDiv clearfix\">	\n" +
-    "			<div class=\"pictureStats clearfix\">\n" +
-    "				<div class=\"mainPicture clearfix\">\n" +
-    "					<h2>42</h2>\n" +
-    "					<img src=\"./images/icons/camerawhite.png\" alt=\"\">\n" +
-    "				</div>\n" +
-    "				<div class=\"threeCatogoryPicture clearfix\">\n" +
-    "					<p>Theme</p>\n" +
-    "					<div class=\"paintings clearfix\">\n" +
-    "						<img class=\"clearfix\" src=\"./images/icons/picturewhite.png\" alt=\"\">\n" +
-    "						<div class=\"statBar bar1 clearfix\"></div>\n" +
-    "						<img class=\"clearfix rightPicture\" src=\"./images/icons/sculpturewhite.png\" alt=\"\">\n" +
-    "					</div>\n" +
-    "					<div class=\"building clearfix\">\n" +
-    "						<img class=\"clearfix\" src=\"./images/icons/buildingwhite.svg\" alt=\"\">\n" +
-    "						<div class=\"statBar bar2 clearfix\"></div>\n" +
-    "						<img class=\"clearfix rightPicture\" src=\"./images/icons/brandblusserwhite.png\" alt=\"\">\n" +
-    "					</div>\n" +
-    "					<div class=\"people clearfix\">\n" +
-    "						<img class=\"clearfix\" src=\"./images/icons/peoplewhite.png\" alt=\"\">\n" +
-    "						<div class=\"statBar bar3 clearfix\"></div>\n" +
-    "					</div>\n" +
-    "				</div>\n" +
-    "			</div>\n" +
-    "			<div class=\"timeStats clearfix\">\n" +
-    "				<div class=\"time clearfix\">\n" +
-    "					<p>Visit:</p>\n" +
-    "					<img src=\"./images/icons/clockwhite.png\" alt=\"\" class=\"clearfix\">\n" +
-    "					<div class=\"timeBar clearfix\">\n" +
-    "						<p>90 min</p>\n" +
-    "					</div>\n" +
-    "				</div>	\n" +
-    "				<div class=\"time clearfix\">\n" +
-    "					<p>Interview:</p>\n" +
-    "					<img src=\"./images/icons/clockwhite.png\" alt=\"\" class=\"clearfix\">\n" +
-    "					<div class=\"timeBar timeBarSmall clearfix\">\n" +
-    "						<p>45 min</p>\n" +
-    "					</div>\n" +
-    "				</div>	\n" +
-    "			</div>\n" +
-    "		</div>\n" +
-    "	</div>\n" +
-    "</section>\n" +
-    "\n" +
-    "<article>\n" +
-    "	<hr>\n" +
-    "	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti cum modi error qui minima nam quae incidunt provident minus et. Neque cumque, totam mollitia illum sed excepturi error expedita. Molestiae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia non totam porro amet possimus accusantium, doloribus esse, est veniam numquam animi. Id, enim distinctio veniam doloribus mollitia. Voluptas, possimus, excepturi!</p>\n" +
+    "	<img class=\"infographic\"></img><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti cum modi error qui minima nam quae incidunt provident minus et. Neque cumque, totam mollitia illum sed excepturi error expedita. Molestiae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia non totam porro amet possimus accusantium, doloribus esse, est veniam numquam animi. Id, enim distinctio veniam doloribus mollitia. Voluptas, possimus, excepturi!</p>\n" +
     "\n" +
     "	<p>Dolor sit amet, consectetur adipisicing elit. Fugiat magni quam recusandae dolores nihil sed molestias quaerat neque corporis iste, dolorem earum aut rem natus atque eius ad animi ut, vitae nemo cum, at incidunt! Dignissimos aperiam, dolor. Sed dicta adipisci reiciendis, eum fugiat dolorem asperiores a ut. Sit assumenda neque eius reprehenderit maxime deleniti voluptate, praesentium error asperiores ratione! Reiciendis cumque obcaecati sint sapiente cum, adipisci, libero dolorem mollitia itaque dolor natus necessitatibus in eaque facilis distinctio quisquam. Ullam debitis porro corrupti, tenetur, distinctio, sunt cum laudantium obcaecati sint, incidunt cupiditate </p>\n" +
     "\n" +
@@ -141,21 +82,6 @@ angular.module("templates/case-study.html", []).run(["$templateCache", function(
     "	<img src=\"./images/backgrounds/librijks.jpg\" alt=\"\">\n" +
     "	<p>Im a real good caption of a picture</p>\n" +
     "</section>\n" +
-    "\n" +
-    "<article>\n" +
-    "	<hr>\n" +
-    "	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti cum modi error qui minima nam quae incidunt provident minus et. Neque cumque, totam mollitia illum sed excepturi error expedita. Molestiae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates, cum, voluptas. Ipsum illum veniam, illo sint reprehenderit, aut tempore debitis voluptatum facere temporibus est minima dolore natus, pariatur sequi labore.</p>\n" +
-    "	<p>Dolor sit amet, consectetur adipisicing elit. Fugiat magni quam recusandae dolores nihil sed molestias quaerat neque corporis iste, dolorem earum aut rem natus atque eius ad animi ut, vitae nemo cum, at incidunt! Dignissimos aperiam, dolor. Sed dicta adipisci reiciendis, eum fugiat dolorem asperiores a ut. Sit assumenda neque eius reprehenderit maxime deleniti voluptate, praesentium error asperiores ratione! Reiciendis cumque obcaecati sint sapiente cum, adipisci, libero dolorem mollitia itaque dolor natus necessitatibus in eaque facilis distinctio quisquam. Ullam debitis porro corrupti, tenetur, distinctio, sunt cum laudantium obcaecati sint, incidunt cupiditate </p>\n" +
-    "	<p>laboriosam soluta architecto culpa aut facilis dolorem commodi! Non beatae delectus facilis quae omnis ipsam incidunt dolorum esse culpa vitae quibusdam minus doloribus consequatur eligendi voluptas facere, fuga reiciendis natus, blanditiis temporibus? Sint dignissimos quas ullam officia, ex vitae perspiciatis reprehenderit. Sit repudiandae asperiores corporis quidem sunt placeat accusantium ea earum libero eius debitis fugit itaque ex id sapiente perspiciatis, sint animi exercitationem iusto cumque quae. \n" +
-    "\n" +
-    "	<img src=\"./images/ucs/1mrB/DSC00129.JPG\">\n" +
-    "\n" +
-    "	Numquam totam illum atque molestiae tempore sed, recusandae minima exercitationem non quibusdam est amet Repellendus ut nemo velit, explicabo. Perspiciatis ratione soluta nihil nam ab voluptatem quos eos excepturi expedita molestiae accusamus delectus dolorum dicta, aspernatur fuga nulla quas, saepe, consectetur odio maiores esse cumque! Vel eaque, expedita doloribus, voluptates rerum molestiae nulla architecto dignissimos, esse porro voluptatibus omnis explicabo? Mollitia, provident sequi eligendi quia temporibus hic ex magnam aliquam nobis, earum omnis porro odit illo dolorum. Nemo esse officiis dolorum distinctio aperiam fugit optio eum non obcaecati, sapiente rem sit tempore facilis, saepe a eaque, totam neque. </p>\n" +
-    "\n" +
-    "	<p>Cupiditate consequatur natus, quidem magnam accusantium quae temporibus corporis repudiandae eaque necessitatibus unde ab quod molestiae, cum tenetur, sint distinctio dolor, numquam officiis reiciendis error! Aliquid illo in assumenda quos repellat! Nobis, voluptate nihil! Tempora, consequatur cumque assumenda non dolorem debitis nam qui ullam repudiandae repellat necessitatibus quia quidem dolore recusandae odio culpa minima et laborum, ad voluptatum excepturi tenetur velit? Ipsam unde vel atque harum sit nulla, aliquam dolores animi voluptatem dolorem quas, vitae doloribus velit earum distinctio nostrum porro ullam error facilis iure aperiam, saepe repellat et. Architecto eum commodi facere ut dolor, voluptate tempore voluptatem vel rerum. Nam hic velit numquam eos quo fugiat modi, sequi aliquam libero, quam qui ipsum doloremque dicta recusandae neque, deleniti ipsam laborum esse minima at dolorum laudantium? At magnam soluta quidem rerum labore, nihil maiores praesentium, eum quas ad rem officiis, laboriosam non placeat minus.</p>\n" +
-    "	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti cum modi error qui minima nam quae incidunt provident minus et. Neque cumque, totam mollitia illum sed excepturi error expedita. Molestiae.</p>\n" +
-    "	<hr>\n" +
-    "</article>\n" +
     "\n" +
     "<section class=\"blackTransition small left\">\n" +
     "	<h2><a href=\"#home\">Return Home</a></p>\n" +
