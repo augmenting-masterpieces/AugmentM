3d69e48d6152aa0e1685472a440a5a8b310a5eb5
Zaturrby <rjkorteschiel@gmail.com>
2014-09-19 20:07:13 +0200

    small changes

--- src/scripts/controllers/homeCtrl.js
+++ src/scripts/controllers/homeCtrl.js
@@ -6,25 +6,25 @@
 	function HomeCtrl(){
 	    var vm = this;
 
-	    spanTextForSkew();
+	   //  spanTextForSkew();
 
-	    function spanTextForSkew() {
-	    	var words = [];
-		    $(".methodsContent p").each(function(){
-		    	words.push($(this).text().split(" "));
-		    });
-		    $(".methodsContent p").empty();
+	   //  function spanTextForSkew() {
+	   //  	var words = [];
+		  //   $(".methodsContent p").each(function(){
+		  //   	words.push($(this).text().split(" "));
+		  //   });
+		  //   $(".methodsContent p").empty();
 
-		    var count = 1;
-		    $(words).each(function(){
-		    	for (var i=0; i<this.length; i++){
-			        if (this[i]!=='') {
-			            $(".methodsContent p:nth-of-type(" + count + " )").append("<span>"+this[i]+"</span> ");
-		        	}
-				}
-				count++;
-		    }); 	
-	    }
+		  //   var count = 1;
+		  //   $(words).each(function(){
+		  //   	for (var i=0; i<this.length; i++){
+			 //        if (this[i]!=='') {
+			 //            $(".methodsContent p:nth-of-type(" + count + " )").append("<span>"+this[i]+"</span> ");
+		  //       	}
+				// }
+				// count++;
+		  //   }); 	
+	   //  }
 
 	    return vm;
 	}
