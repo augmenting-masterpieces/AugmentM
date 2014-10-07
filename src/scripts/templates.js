62afb3e4cf0e78cc28e608485be5ab4e6d1637ca
Zaturrby <rjkorteschiel@gmail.com>
2014-10-07 17:04:17 +0200

    made 90degree angle

--- src/scripts/templates.js
+++ src/scripts/templates.js
@@ -2,67 +2,61 @@ angular.module('templates-cth', ['templates/case-study.html', 'templates/home.ht
 
 angular.module("templates/case-study.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("templates/case-study.html",
-    "<section class=\"caseTop\">\n" +
-    "	<h1><a href=\"#case-study\">U<span>SER </span>C<span>AMERA </span>S<span>TUDY </span></a></h1>\n" +
-    "	<h2>“When I have a camera in my hand, I dare to walk in front of a painting. It's like having permission to crawl through the masses.” - participant UCS</h2>\n" +
-    "	<h1 class=\"arrow\">&#8595; </h1>\n" +
-    "</section>\n" +
+    "<section class=\"caseWrapper\">\n" +
+    "	<section class=\"caseTop\">\n" +
+    "		<h1><a href=\"#case-study\">U<span>SER </span>C<span>AMERA </span>S<span>TUDY </span></a></h1>\n" +
+    "		<h2>“When I have a camera in my hand, I dare to walk in front of a painting. It's like having permission to crawl through the masses.” - participant UCS</h2>\n" +
+    "		<h1 class=\"arrow\">&#8595; </h1>\n" +
+    "	</section>\n" +
     "\n" +
-    "<section class=\"blackTransition small\">\n" +
-    "</section>\n" +
+    "	<section class=\"topdiagonal\">\n" +
+    "	</section>\n" +
     "\n" +
-    "<article >\n" +
-    "	<hr>\n" +
-    "	<h3> How do visitors experience the Rijksmuseum?</h3>\n" +
-    "	<hr>\n" +
-    "	<p class=\"boldAlinea\">This case study shows how the User Camera Studies were conducted, gives visual examples and types of insights a museum can gain by using photographic means as a source of feedback from their visitors.</p>\n" +
+    "	<section class=\"textblock1\" >\n" +
+    "		<h3> How do visitors experience the Rijksmuseum?</h3>\n" +
+    "		<p class=\"boldAlinea\">This case study shows how the User Camera Studies were conducted, gives visual examples and types of insights a museum can gain by using photographic means as a source of feedback from their visitors.</p>\n" +
     "\n" +
-    "	<p>Augmenting Masterpieces applies visitor's photography through User Camera Studies (UCS)1 as one of the main research sources. In addition to other interview methods they give not only room to individual oral, but also to visual stories and analysis which supports the project's targeted innovation process for multi-medial and digital interfaces for museums. \n" +
-    "	</p>\n" +
+    "		<p>Augmenting Masterpieces applies visitor's photography through User Camera Studies (UCS)1 as one of the main research sources. In addition to other interview methods they give not only room to individual oral, but also to visual stories and analysis which supports the project's targeted innovation process for multi-medial and digital interfaces for museums. \n" +
+    "		</p>\n" +
     "\n" +
-    "	<p>The UCS helped to understand the social dimensions of a museum and map the museum experience as the participating visitors moved social aspects and rather basic preconditions for art reception back into the center of attention by addressing their interaction with the space, museum staff, other visitors and art objects. </p>\n" +
+    "		<p>The UCS helped to understand the social dimensions of a museum and map the museum experience as the participating visitors moved social aspects and rather basic preconditions for art reception back into the center of attention by addressing their interaction with the space, museum staff, other visitors and art objects. </p>\n" +
     "\n" +
-    "	<p></p>\n" +
-    "</article>\n" +
-    "<section class=\"infoContainer\">\n" +
-    "	<hr>\n" +
-    "	<img src=\"./images/ucs/1mrB/visual_nieuw150.jpg\"class=\"infographic\"></img>\n" +
-    "	<hr>\n" +
-    "</section>\n" +
-    "<article>\n" +
-    "	<p>The purpose of our study was to get qualitative input from the visitors for developing digital devices and interactive means in the Rijksmuseum. The User Camera Study consists of two phases. First the participants are given a camera with which they walk through the museum and take pictures of whatever draws their attention. They can choose themselves what they capture, for how long they want to stay in the exhibition and how many pictures they want to take.</p>\n" +
+    "		<p></p>\n" +
+    "	</section>\n" +
+    "	<section class=\"textblock2\">\n" +
+    "		<p>The purpose of our study was to get qualitative input from the visitors for developing digital devices and interactive means in the Rijksmuseum. The User Camera Study consists of two phases. First the participants are given a camera with which they walk through the museum and take pictures of whatever draws their attention. They can choose themselves what they capture, for how long they want to stay in the exhibition and how many pictures they want to take.</p>\n" +
     "\n" +
-    "	<p>The second phase is a qualitative interview in which they first get to talk about the photographs in a way that is comparable to someone showing their vacation pictures: descriptive and as detailed as they prefer. Later on specific questions concerning sensual experiences (what did you hear, smell, touch?) and digital devices (which devices did you use during your visit and to do what?) </p>\n" +
+    "		<p>The second phase is a qualitative interview in which they first get to talk about the photographs in a way that is comparable to someone showing their vacation pictures: descriptive and as detailed as they prefer. Later on specific questions concerning sensual experiences (what did you hear, smell, touch?) and digital devices (which devices did you use during your visit and to do what?) </p>\n" +
     "\n" +
-    "	<p>This setting gives the participants freedom to talk about their own points of interest and observations during their visit in a vacation-style narrative that is familiar to most people. The pictures that the participants take also serve to remember certain situations and aspects easily. As the study provides oral and visual feedback, it reveals different kinds of information than a thematically focused survery. </p>\n" +
+    "		<p>This setting gives the participants freedom to talk about their own points of interest and observations during their visit in a vacation-style narrative that is familiar to most people. The pictures that the participants take also serve to remember certain situations and aspects easily. As the study provides oral and visual feedback, it reveals different kinds of information than a thematically focused survery. </p>\n" +
     "\n" +
-    "	<p>The survey had twelve participants from four out of the five visitors' categories the Rijksmuseum works with on a daily basis in their education and marketing department. These four categories all concern unaccompanied visitors (Cultural Tourist, Art Lover's, Cultural Professionals and Families, whereas the fifth category consists of accompanied groups (school classes and workshop groups from the Teekenschool).</p>\n" +
-    "	<p>Three Cultural Tourists, three Art Lover's, two Cultural Professionals and one Family (with three children, who each participated as well) participated in the present survey. Below there is one detailed profile and script from a participating Cultural Tourist in order to get an indepth view on the kind of feedback provided.The most revealing result of the survey were that the social dimensions of a visit to the Rijksmuseum are highly important for all visitor's types. Whereas the amount of time spent on looking at art works and written information varied largely, all participants observed other visitors, listened into their conversations or were wondering about their own – proper - behaviour at times. </p>\n" +
-    "	<hr>\n" +
-    "</article>\n" +
+    "		<p>The survey had twelve participants from four out of the five visitors' categories the Rijksmuseum works with on a daily basis in their education and marketing department. These four categories all concern unaccompanied visitors (Cultural Tourist, Art Lover's, Cultural Professionals and Families, whereas the fifth category consists of accompanied groups (school classes and workshop groups from the Teekenschool).</p>\n" +
+    "		<p>Three Cultural Tourists, three Art Lover's, two Cultural Professionals and one Family (with three children, who each participated as well) participated in the present survey. Below there is one detailed profile and script from a participating Cultural Tourist in order to get an indepth view on the kind of feedback provided.The most revealing result of the survey were that the social dimensions of a visit to the Rijksmuseum are highly important for all visitor's types. Whereas the amount of time spent on looking at art works and written information varied largely, all participants observed other visitors, listened into their conversations or were wondering about their own – proper - behaviour at times. </p>\n" +
+    "	</section>\n" +
     "\n" +
-    "<section class=\"blackTransition small left\">\n" +
-    "	<h1>Full interview</h1>\n" +
-    "</section>\n" +
+    "	<section class=\"blackTransition small left\">\n" +
+    "		<h3>Full interview</h3>\n" +
+    "	</section>\n" +
     "\n" +
-    "<section class=\"pictureGallery\">\n" +
-    "	<div class=\"top\">\n" +
-    "		<div class=\"conversation clearfix\">\n" +
-    "			<hr>\n" +
-    "			<p>Het vliegtuig is alleen maar bijzonder omdat er een vliegtuig staat op n plek waar op geen enkele manier een vliegtuig binnen kan komen. Je weet niet hoe het daar is gekomen, je begrijpt alleen dat het heel lastig wordt als je de tentoonstelling ooit wilt veranderen. Om het er weer uit te krijgen. Aan het begin vond ik het in deze zaal heel fijn dat er muziek op de achtergrond is omdat alles is stil en iedereen is soort van panisch geruisloos en.. gehoorsam. Later gingen wij naar de etsen en schetsen en dan stoort het mij wel weer. Dat het daar aanstaat. Want het heeft een betrekking tot een totaal andere gedeelte van die vloer waardoor het eigenlijk op dat moment van functioneel naar niet functioneel gaat.<br><br>\n" +
-    "			Title: Airplane<br>\n" +
-    "			Tag: object (exhibit)</p>\n" +
-    "			<hr>\n" +
+    "	<section class=\"pictureGallery\">\n" +
+    "		<div class=\"top\">\n" +
+    "			<div class=\"conversation clearfix\">\n" +
+    "				<hr>\n" +
+    "				<p>Het vliegtuig is alleen maar bijzonder omdat er een vliegtuig staat op n plek waar op geen enkele manier een vliegtuig binnen kan komen. Je weet niet hoe het daar is gekomen, je begrijpt alleen dat het heel lastig wordt als je de tentoonstelling ooit wilt veranderen. Om het er weer uit te krijgen. Aan het begin vond ik het in deze zaal heel fijn dat er muziek op de achtergrond is omdat alles is stil en iedereen is soort van panisch geruisloos en.. gehoorsam. Later gingen wij naar de etsen en schetsen en dan stoort het mij wel weer. Dat het daar aanstaat. Want het heeft een betrekking tot een totaal andere gedeelte van die vloer waardoor het eigenlijk op dat moment van functioneel naar niet functioneel gaat.<br><br>\n" +
+    "				Title: Airplane<br>\n" +
+    "				Tag: object (exhibit)</p>\n" +
+    "				<hr>\n" +
+    "			</div>\n" +
+    "			<h2 class=\"galleryQuote clearfix\">\"...alles is stil en iedereen is soort van panisch geruisloos en.. gehoorsam.\"</h2>\n" +
     "		</div>\n" +
-    "		<h2 class=\"galleryQuote clearfix\">\"...alles is stil en iedereen is soort van panisch geruisloos en.. gehoorsam.\"</h2>\n" +
-    "	</div>\n" +
-    "	<div class=\"bottom\">\n" +
-    "		<div id=\"galleryCaseStudy\" class=\"owl-carousel owlGallery\"></div>	\n" +
-    "	</div>\n" +
-    "</section>\n" +
+    "		<div class=\"bottom\">\n" +
+    "			<div id=\"galleryCaseStudy\" class=\"owl-carousel owlGallery\"></div>	\n" +
+    "		</div>\n" +
+    "	</section>\n" +
     "\n" +
-    "<section class=\"blackTransition small left\">\n" +
-    "	<h2><a href=\"#home\">Return Home</a></p>\n" +
+    "	<section class=\"blackTransition small left\">\n" +
+    "		<h4><a href=\"#home\">Return Home</a></h4>\n" +
+    "	</section>\n" +
     "</section>\n" +
     "");
 }]);
@@ -256,7 +250,7 @@ angular.module("templates/home.html", []).run(["$templateCache", function($templ
     "	</section>\n" +
     "	<section class=\"homeConnect\">\n" +
     "		<div class=\"title\">\n" +
-    "			<h1>A<span>UGMENTING</span><span class=\"second\">&nbsp;M</span><span>ASTERPIECES &#8594;</span> </h1>\n" +
+    "			<h1>A<span>UGMENTING</span><span class=\"second\">&nbsp;M</span><span>ASTERPIECES</span> </h1>\n" +
     "			<!-- <h2>...through embedded research and human-centered design.</h2 > -->\n" +
     "\n" +
     "			<p>Augmenting Masterpieces explores visitors' experiences and the social dimensions of a visit to the Rijksmuseum. It translates the findings into an interface which lets the visitor interact with both the physical and the digital collection.\n" +
@@ -266,8 +260,6 @@ angular.module("templates/home.html", []).run(["$templateCache", function($templ
     "				<a href=\"http://cdh.uva.nl/\"><img src=\"./images/logos/uvalogowhite.png\"></a>\n" +
     "			</div>\n" +
     "		</div>\n" +
-    "\n" +
-    "\n" +
     "	</section>\n" +
     "</section>\n" +
     "\n" +
