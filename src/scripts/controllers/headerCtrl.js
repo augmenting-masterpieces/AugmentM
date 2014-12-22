435dc5285fc1266c135001575f85babeb42a1cd7
Zaturrby <rjkorteschiel@gmail.com>
2014-12-22 13:19:43 +0100

    anim tryout #34

--- src/scripts/controllers/headerCtrl.js
+++ src/scripts/controllers/headerCtrl.js
@@ -9,12 +9,12 @@
     // console.log($(".fadeclass")[0]);
 
     $(".fadeclass").map(function(){
-    	var time = Math.random() * (7000 - 2000) + 2000;
-    	$(this).hide().fadeIn(time);
+    	var time = Math.random() * (6000 - 2000) + 2000;
+        $(this).fadeTo(1, 0).delay(time).fadeTo(1000, 1);
     });
 
-    $(".logos").hide().fadeIn(5000);
-    $(".headerText").hide().fadeIn(5000);
+    $(".logos").hide().delay(7000).fadeIn(1000);
+    $(".headerText").hide().delay(7000).fadeIn(1000);
 
 
 	return vm;
