988e8b42e18221fc986ec860e4a317ddb9408d70
Zaturrby <rjkorteschiel@gmail.com>
2014-12-22 10:13:00 +0100

    headeranim #34

--- src/scripts/controllers/headerCtrl.js
+++ src/scripts/controllers/headerCtrl.js
@@ -6,9 +6,16 @@
 	function HeaderCtrl($scope){
     var vm = this;
 
-    console.log($(".fadeclass")[0]);
+    // console.log($(".fadeclass")[0]);
 
-    $(".fadeclass")[0].hide();
+    $(".fadeclass").map(function(){
+    	var time = Math.random() * (7000 - 2000) + 2000;
+    	$(this).hide().fadeIn(time);
+    });
+
+    $(".logos").hide().fadeIn(5000);
+    $(".headerText").hide().fadeIn(5000);
+
 
 	return vm;
 	}
