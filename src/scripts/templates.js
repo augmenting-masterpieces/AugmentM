a87849d1517fb5e7c6f351f34b3a5bfccfe1ec82
Zaturrby <rjkorteschiel@gmail.com>
2015-01-06 12:49:34 +0100

    #48 template processmap disconnected from ui-router

--- src/scripts/templates.js
+++ src/scripts/templates.js
@@ -384,10 +384,10 @@ angular.module("../templates/content/usercamerastudy.html", []).run(["$templateC
 angular.module("../templates/displayorder.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("../templates/displayorder.html",
     "<section ui-view=\"header\" class=\"row block maintro\"></section>\n" +
-    "<section ui-view=\"processmap\" class=\"row block processmap\"></section>\n" +
+    "<!-- <section ui-view=\"processmap\" class=\"row block processmap\"></section>\n" +
     "<section class=\"row block intermezzo understand\">\n" +
     "	<h1 id=\"understand\" class=\"col-lg-9 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-11 col-sm-offset-1 col-xs-24 col-xs-offset-0\"><span>Understand</span> and <span>Observe</span> </h1>\n" +
-    "</section>\n" +
+    "</section> -->\n" +
     "<section ui-view=\"assumptions\" class=\"row block assumptions\"></section>\n" +
     "<section ui-view=\"usercamerastudy\" class=\"row block userCameraStudy\"></section>\n" +
     "<section class=\"row block intermezzo analyse\">\n" +
